b244b338c51beb039b3e074a015dc1f44de66bd5
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-05 12:17:42 -0200

    refactoring

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -81,13 +81,6 @@ export default {
       allColors: '',
       sizes: [],
       prices: '',
-      pricesList: [
-        { label: 'de R$ 0 até R$ 50', value: '0-50' },
-        { label: 'de R$ 51 até R$ 150', value: '51-150' },
-        { label: 'de R$ 151 até R$ 300', value: '151-300' },
-        { label: 'de R$ 301 até R$ 500', value: '301-500' },
-        { label: 'a partir de R$ 501', value: '501' }
-      ],
       dresses: null,
       allDresses: null,
       showLoaderButton: true
